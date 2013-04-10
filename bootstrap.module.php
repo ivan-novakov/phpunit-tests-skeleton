35d724fcd31903651947dce6eca3f0c9ec3c7457
Ivan Novakov <ivan.novakov@fel.cvut.cz>
2013-04-10 10:55:55 +0200

    initial

---
+++ bootstrap.module.php
@@ -0,0 +1,16 @@
+<?php
+use Prefix\Module;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+require __DIR__ . '/../Module.php';
+
+define('TESTS_DATA_DIR', __DIR__ . '/data');
+
+$module = new Module();
+\Zend\Loader\AutoloaderFactory::factory($module->getAutoloaderConfig());
+
+//----------
+function _dump($value)
+{
+    error_log(print_r($value, true));
+}
